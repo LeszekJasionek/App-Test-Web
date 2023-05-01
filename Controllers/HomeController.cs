a565b8aaec473faf9457d9bba60e7f1d3f7204cb
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-05-01 12:24:21 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using App_Test_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace App_Test_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace App_Test_Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
