018d5b9e831edd1242cda23528c47fd43eb53bfb
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-05-01 14:37:32 +0200

    How the razor view engine works

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App_Test_Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace App_Test_Web.Controllers
 {
@@ -8,5 +9,60 @@ namespace App_Test_Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Product()
+        {
+            var product = new Product
+            {
+                Id = 1,
+                Category = "Cars",
+                Description = "It's a great car",
+                Name = "Audi"
+            };
+
+            return View(product);
+
+        }
+
+        public IActionResult List()
+        {
+            var productList = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Category = "Cars",
+                    Description = "It's a great car",
+                    Name = "Audi"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Category = "Drinks",
+                    Description = "It's very good",
+                    Name = "PEPSI"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Category = "Plants",
+                    Description = "Very popular plant",
+                    Name = "Cactus"
+                }
+            };
+
+            return View(productList);
+        }
+
+        public IActionResult Data()
+        {
+            ViewBag.Name = "Leszek";
+            ViewData["Surname"] = "Jasionek";
+            TempData["SecondName"] = "Piotr";
+
+
+            return View();
+        }
+
     }
 }
