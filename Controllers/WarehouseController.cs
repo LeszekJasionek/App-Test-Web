fea363966a726684a0fb90450d57546fea1352f0
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-05-01 15:17:01 +0200

    HTTP request forms and headers?

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,31 @@
+﻿using App_Test_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App_Test_Web.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product body)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(body);
+            }
+
+            return RedirectToAction("Add");
+        }
+
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,15 @@
-﻿namespace App_Test_Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App_Test_Web.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Het veld 'Name' is verplicht ")]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Category { get; set; }
     }
 }
